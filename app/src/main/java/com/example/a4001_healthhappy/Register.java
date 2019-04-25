85dcb4c12ca3bb69605c3306c41e6dcb9da40511
Annisa Dwiayu Ramadhanty <annisadwiayu@gmail.com>
2019-04-26 00:48:58 +0700

    'register'

--- app/src/main/java/com/example/a4001_healthhappy/Register.java
+++ app/src/main/java/com/example/a4001_healthhappy/Register.java
@@ -1,9 +1,22 @@
 package com.example.a4001_healthhappy;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+
+    private EditText emailid, passid;
+    private FirebaseAuth mAuth ;
+    private FirebaseUser user;
+    private static final String TAG = "";
+    private ProgressBar progressBar;
+    private ProgressDialog pbDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
