c75ae5cf91c323b24040b37d984922f977fab147
Erny Nursetyawati <ernynursetyawati98@gmail.com>
2019-04-26 11:06:58 +0700

    'initial'

---
+++ app/src/main/java/com/example/a4001_healthhappy/Diet/DietKeto.java
@@ -0,0 +1,22 @@
+package com.example.a4001_healthhappy.Diet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.a4001_healthhappy.R;
+
+public class DietKeto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_keto);
+    }
+
+    public void intentKeto (View view) {
+        Intent intent = new Intent(DietKeto.this, DietKeto.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/a4001_healthhappy/Diet/DietMayo.java
@@ -0,0 +1,21 @@
+package com.example.a4001_healthhappy.Diet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.a4001_healthhappy.R;
+
+public class DietMayo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_mayo);
+    }
+    public void intentMayo (View view) {
+        Intent intent = new Intent(DietMayo.this, DietMayo.class);
+        startActivity(intent);
+    }
+}
