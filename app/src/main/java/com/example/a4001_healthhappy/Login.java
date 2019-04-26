145dd16a780ad9eb0df37b6dadb5dd0851e4bc80
Annisa Dwiayu Ramadhanty <annisadwiayu@gmail.com>
2019-04-26 10:29:51 +0700

    'init'

--- app/src/main/java/com/example/a4001_healthhappy/Login.java
+++ app/src/main/java/com/example/a4001_healthhappy/Login.java
@@ -1,4 +1,4 @@
-package com.example.a4001_healthhappy.Program;
+package com.example.a4001_healthhappy;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -10,10 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.a4001_healthhappy.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
