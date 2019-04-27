77465f00a5f61ded843fd3219327b90018b24757
tannimaisari <tannimaisari9@gmail.com>
2019-04-27 21:39:53 +0700

    DetailTodo(1)

---
+++ app/src/main/java/com/example/a4001_healthhappy/DetailTodo.java
@@ -0,0 +1,18 @@
+package com.example.a4001_healthhappy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class DetailTodo extends AppCompatActivity {
+
+    TextView jdlDtl, descDtl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_todo);
+
+
+    }
+}
