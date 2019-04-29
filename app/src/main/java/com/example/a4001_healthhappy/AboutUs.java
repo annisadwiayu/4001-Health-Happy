e467651e3028b8557ac6f8a846e93918b8e6cde7
Anindya Tyas <anindyatyasw@gmail.com>
2019-04-29 13:46:13 +0700

    'list'

--- app/src/main/java/com/example/a4001_healthhappy/AboutUs.java
+++ app/src/main/java/com/example/a4001_healthhappy/AboutUs.java
@@ -11,6 +11,7 @@ public class AboutUs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
         AboutUs.this.setTitle("About Us");
